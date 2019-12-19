84b63e6a5157f4aac089ec32eca60e72a80119ce
ivers523 <ivers523@umn.edu>
2019-12-18 22:26:59 -0600

    implemented ADD function set

--- server.js
+++ server.js
@@ -29,12 +29,15 @@ function runPrompt() {
       name: "start",
       message: "What would you like to do?",
       choices: [
+        new inquirer.Separator(),
         "View departments",
         "View roles",
         "View employees",
+        new inquirer.Separator(),
         "Add department",
         "Add role",
         "Add employee",
+        new inquirer.Separator(),
         "Exit"
       ]
     })
@@ -75,75 +78,164 @@ function runPrompt() {
 // Create fxns for each case. should be recursive.
 // IMPLEMENT "BACK" function
 function viewDepartments() {
-  inquirer
-    .prompt({
-      name: "inputdepartment",
-      type: "input",
-      message: "Please input a department name:"
-    })
-    .then(function (res) {
-      const query = connection.query("SELECT * FROM department WHERE name=?", [`${res.inputdepartment}`], function (err, res) {
-        if (err) throw err;
-        for (let i = 0; i < res.length; i++) {
-          console.table(res);
-
-        }
-      }
-      )
-    }
-    )
+  const query = connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runPrompt();
+  }
+  )
 }
 
 function viewRoles() {
-  inquirer
-    .prompt({
-      name: "inputrole",
-      type: "input",
-      message: "Please input a role title:"
-    })
-    .then(function (res) {
-      const query = connection.query("SELECT * FROM role WHERE title=?", [`${res.inputrole}`], function (err, res) {
-        if (err) throw err;
-        for (let i = 0; i < res.length; i++) {
-          console.table(res);
+  const query = connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runPrompt();
 
-        }
-      }
-      )
-    }
-    )
+  }
+  )
 }
 
 function viewEmployees() {
-  inquirer
-    .prompt({
-      name: "inputemployee",
-      type: "input",
-      message: "Please input an employee id:"
-    })
-    .then(function (res) {
-      const query = connection.query("SELECT * FROM employee WHERE id=?", [`${res.inputemployee}`], function (err, res) {
-        if (err) throw err;
-        for (let i = 0; i < res.length; i++) {
-          console.table(res);
-
-        }
-      }
-      )
-    }
-    )
+  const query = connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runPrompt();
+  }
+  )
 }
 
 // ADD function
-// example for adding data
-// function addRow() {
-//     connection.query(`INSERT INTO items(name, price, category) VALUES("${name}", ${price}, "${category}");`, function (err, res) {
-//         if (err) {
-//             console.log(err)
-//         }
-//         console.log('Item added!! Bringing you back to home page.');
-//         console.log("-----------------------------------");
 
-//         firstPrompt();
-//     });
-// }
+// function to adding
+function addDepartment() {
+  // prompt for department keys
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "input",
+        message: "What is the ID of the department you would like to add?"
+      },
+      {
+        name: "name",
+        type: "input",
+        message: "What is the name of the department you would like to add?"
+      }
+    ])
+    .then(function (res) {
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          id: res.id,
+          name: res.name,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Your department was created!");
+          // re-prompt the user 
+          runPrompt();
+        }
+      );
+    });
+}
+
+function addRole() {
+  // prompt for role keys
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "input",
+        message: "What is the ID of the role you would like to add?"
+      },
+      {
+        name: "title",
+        type: "input",
+        message: "What is the title of the department you would like to add?"
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the annual salary of the role you would like to add?"
+      },
+      {
+        name: "departmentid",
+        type: "input",
+        message: "What is the department ID of the role you would like to add?"
+      }
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          id: res.id,
+          title: res.title,
+          salary: res.salary,
+          department_id: res.departmentid
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Your role was created!");
+          // return to prompt
+          runPrompt();
+        }
+      );
+    });
+}
+
+function addEmployee() {
+  // prompt for role keys
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "input",
+        message: "What is the ID number of the employee you would like to add?"
+      },
+      {
+        name: "firstname",
+        type: "input",
+        message: "What is the first name of the employee you would like to add?"
+      },
+      {
+        name: "lastname",
+        type: "input",
+        message: "What is the last name of the employee you would like to add?"
+      },
+      {
+        name: "roleid",
+        type: "input",
+        message: "What is the role ID of the employee you would like to add?"
+      },
+      {
+        name: "managerid",
+        type: "input",
+        message: "If your employee has a manager, please enter their ID number"
+      }
+
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          id: res.id,
+          first_name: res.firstname,
+          last_name: res.lastname,
+          role_id: res.roleid,
+          manager_id: res.managerid
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Your employee was created!");
+          // return to prompt
+          runPrompt();
+        }
+      );
+    });
+}
+
+
+
+
